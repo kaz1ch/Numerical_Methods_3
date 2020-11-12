5adb5044ba9e6d52cea18dd1921b323a4995cdd7
Илья Казютин <gorgutz1@yandex.ru>
2020-11-12 18:02:44 +0300

    Добавьте файлы проекта.

---
+++ Numerical_Methods_3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numerical_Methods_3
+{
+    public partial class Form1 : Form
+    {
+        const double Pi = 3.14;
+        const double h = Pi / 7;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double f(double x)
+        {
+            return Math.Sin(x) * Math.Sqrt(x) + 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
